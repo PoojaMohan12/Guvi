198fbc260b147b76229fdce844a04c2d5e1c18fa
PoojaMohan12 <51323400+PoojaMohan12@users.noreply.github.com>
2019-06-19 12:38:47 +0530

    Update 14.py

--- Codes/14.py
+++ Codes/14.py
@@ -1,7 +1,7 @@
-a1=eval(input())
-a2=eval(input())
-l=[]
-for i in range (a1,a2):
+a1=int(input())
+b1=int(input())
+lis=[]
+for i in range (a1,b1):
     if (i%2!=0):
-        l.append(i)
-print(*l)
+        lis.append(i)
+print(*lis)
