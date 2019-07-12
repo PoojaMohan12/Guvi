0332655c0444c6eefbc8ae4ba4b1b968f34dac93
PoojaMohan12 <51323400+PoojaMohan12@users.noreply.github.com>
2019-07-12 22:16:17 +0530

    Update 5.py

--- Codes/player/5.py
+++ Codes/player/5.py
@@ -1,10 +1,20 @@
 n=input()
 s=0
 for i in range (len(n)):
-    if(n[i]=="V" ):
-        s=s+5
-    elif(n[i]=='I'):
-        s=s+1
-    elif(n[i]=='X'):
-        s=s+10
+    if(n=='IV'):
+        s=4
+    elif(n=='IX'):
+        s=9
+    elif(n=='XIV'):
+        s=14
+    elif(n=='XIX'):
+        s=19
+
+    else:
+        if(n[i]=="V" ):
+            s=s+5
+        elif(n[i]=='I'):
+            s=s+1
+        elif(n[i]=='X'):
+            s=s+10
 print(s)
