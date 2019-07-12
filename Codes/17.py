e5f46ba915703c67c21dbcd740e27d31b9c1c70c
PoojaMohan12 <51323400+PoojaMohan12@users.noreply.github.com>
2019-07-12 20:26:20 +0530

    Update 17.py

--- Codes/17.py
+++ Codes/17.py
@@ -1,12 +1,12 @@
-n=input()
-a=int(n[0])
-b=int(n[1])
-c=int(n[2])
-a1=(a*100)+(b*10)+(c*1)
-a2=(a**3)+(b**3)+(c**3)
-if(a1==a2):
-    print('yes')
+n=int(input())
+t=n
+sum=0
+while(n>0):
+    d=n%10
+    sum=sum+d**3
+    n=n//10
+if(t==sum):
+    print("yes")
 else:
-    print("no")
+    print('no')
 
-        
