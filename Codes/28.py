edf89ad5e1342b85dc88ebbe590b6d56183c62ab
PoojaMohan12 <51323400+PoojaMohan12@users.noreply.github.com>
2019-07-15 20:22:45 +0530

    Update 28.py

--- Codes/28.py
+++ Codes/28.py
@@ -1,4 +1,4 @@
-n=int(input())
-a=list(map(int,input().split()))[:n]
-for i in range (n):
-    print(a[i]," ",i)
+num=int(input())
+va=list(map(int,input().split()))[:n]
+for i in range (num):
+    print(va[i],i)
