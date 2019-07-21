3a12dd22619cc051120d03ed572f5d54f59e7868
PoojaMohan12 <51323400+PoojaMohan12@users.noreply.github.com>
2019-07-21 17:17:10 +0530

    Create 7.py

---
+++ Codes/hunter/7.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+o=[]
+for i in range(0,n):
+    if(i%2==0):
+        if(l[i]%2!=0):
+            o.append(l[i])
+    else:
+        if(l[i]%2==0):
+            o.append(l[i])
+print(*o,sep="")
