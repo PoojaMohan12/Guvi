70d7fd4e4c7b28aa76bd826af48dd1f365525ef2
PoojaMohan12 <51323400+PoojaMohan12@users.noreply.github.com>
2019-07-21 17:42:24 +0530

    Create 9.py

---
+++ Codes/hunter/9.py
@@ -0,0 +1,21 @@
+n=int(input())
+l=list(map(int,input().split()))
+p=[]
+z=l.count(0)
+for i in range(n):
+    for j in range (n):
+        if (l[i]+l[j]==0):
+            if((l[i] not in p) and( l[j] not in p)):
+                p.append(l[i])
+                p.append(l[j])
+k=p.count(0)
+if(z==k):
+    print(*p)
+else:
+    for h in range (abs(z-k)):
+        p.remove(0)
+    if(p[-1]==0):
+        p.remove(0)
+    print(*p)
+            
+            
