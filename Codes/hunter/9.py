7c9e89e6f4c05062c8b17297c59807d79af7f73a
PoojaMohan12 <51323400+PoojaMohan12@users.noreply.github.com>
2019-07-21 17:56:47 +0530

    Update 9.py

--- Codes/hunter/9.py
+++ Codes/hunter/9.py
@@ -1,10 +1,10 @@
 si=int(input())
 ti=list(map(int,input().split()))
 ci=max(ti)
-di,ei=0,0
+d,e=0,0
 for i in range(0,len(ti)-1):
   for j in range(i+1,len(ti)):
     if abs(ti[i]+ti[j])<ci:
-      di,ei=ti[i],ti[j]
-      c1=abs(di+ei)
-print(di,ei)
+      d,e=ti[i],ti[j]
+      ci=abs(di+ei)
+print(d,e)
